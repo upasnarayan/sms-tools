0551f2049ef6b62d3fa6ce2979864f7d48458693
Upas Narayan <unaraya1@jhu.edu>
2015-10-04 13:57:07 -0700

    adding finished A1 and incomplete A2

---
+++ workspace/A1/A1Part2.py
@@ -0,0 +1,30 @@
+import sys
+import os
+sys.path.append('../../software/models/')
+from utilFunctions import wavread
+import numpy as np
+
+"""
+A1-Part-2: Basic operations with audio
+
+Write a function that reads an audio file and returns the minimum and the maximum values of the audio 
+samples in that file. 
+
+The input to the function is the wav file name (including the path) and the output should be two floating 
+point values returned as a tuple.
+
+If you run your code using oboe-A4.wav as the input, the function should return the following output:  
+(-0.83486432, 0.56501967)
+"""
+def minMaxAudio(inputFile):
+    """
+    Input:
+        inputFile: file name of the wav file (including path)
+    Output:
+        A tuple of the minimum and the maximum value of the audio samples, like: (min_val, max_val)
+    """
+    
+    fs, x = wavread(inputFile)
+    minVal = np.min(x)
+    maxVal = np.max(x)
+    return (minVal, maxVal)
